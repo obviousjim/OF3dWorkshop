a82b92e0c1c3e8e5a988889f8332fce2c52f43ab
obviousjim <james@jamesgeorge.org>
2011-09-22 13:24:32 +0200

    added 3d model loader example
    Signed-off-by: obviousjim <james@jamesgeorge.org>

--- 04_ModelExample/src/testApp.cpp
+++ 04_ModelExample/src/testApp.cpp
@@ -3,6 +3,16 @@
 //--------------------------------------------------------------
 void testApp::setup(){
 
+	ofBackground(50, 50, 50, 0);
+
+    ofDisableArbTex();
+	glEnable(GL_DEPTH_TEST);
+	
+    //some model / light stuff
+    glShadeModel(GL_SMOOTH);
+    light.enable();
+	model.loadModel("squirrel/NewSquirrel.3ds");
+	model.setPosition(ofGetWidth()*.5, ofGetHeight() * 0.75, 0);
 }
 
 //--------------------------------------------------------------
@@ -12,7 +22,67 @@ void testApp::update(){
 
 //--------------------------------------------------------------
 void testApp::draw(){
+    ofSetColor(255, 255, 255, 255);
+	
+	//drawWithModel();
+	drawWithMesh();
+}
 
+void testApp::drawWithModel(){
+	ofVec3f sceneCenter = model.getSceneCenter();
+	ofVec3f position = model.getPosition();
+
+	ofPushMatrix();
+	ofTranslate(position);
+	ofRotate(-ofGetMouseX(), 0, 1, 0);
+	ofRotate(90,1,0,0);
+	ofTranslate(-position);
+	
+	model.drawFaces();
+	
+    ofPopMatrix();
+	
+}
+
+void testApp::drawWithMesh(){
+
+	ofVec3f sceneCenter = model.getSceneCenter();
+	ofVec3f scale = model.getScale();
+	ofVec3f position = model.getPosition();
+	
+	float normalizedScale = model.getNormalizedScale();
+	
+	ofVboMesh mesh = model.getMesh(0);
+	ofTexture tex = model.getTextureForMesh(0);
+	ofMaterial mat = model.getMaterialForMesh(0);
+	
+	glPushAttrib(GL_ALL_ATTRIB_BITS);
+    glPushClientAttrib(GL_CLIENT_ALL_ATTRIB_BITS);
+    glEnable(GL_NORMALIZE);
+	
+    ofPushMatrix();
+	
+	//cout << sceneCenter << endl;
+	
+	ofTranslate(position);
+	ofRotate(-mouseX, 0, 1, 0);
+	ofRotate(90,1,0,0);
+	
+	
+	ofScale(normalizedScale, normalizedScale, normalizedScale);
+	ofScale(scale.x,scale.y,scale.z);
+	
+	tex.bind();
+	mat.begin();
+	mesh.drawWireframe();
+	mat.end();
+	tex.unbind();
+	
+	ofPopMatrix();
+	
+	glPopAttrib();
+	glPopClientAttrib();
+	
 }
 
 //--------------------------------------------------------------
